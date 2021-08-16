33b74e43dd1ff74d99197ee00e8f4f0f8f1a559b
Dvornick <mail@dvornick.ru>
2021-08-16 21:02:38 +0300

    Improving the search block with javascript

--- build/js/script.js
+++ build/js/script.js
@@ -37,4 +37,15 @@ $(document).ready(function () {
             }
         }
     });
+
+    const iconSearch = document.querySelector('.icons__search');
+    const body = document.querySelector('body');
+
+    iconSearch.addEventListener('click', function (event) {
+        event.stopPropagation();
+        this.classList.add('icons__search_active')
+    });
+    body.addEventListener('click', function (event) {
+        iconSearch.classList.remove('icons__search_active')
+    });
 });
